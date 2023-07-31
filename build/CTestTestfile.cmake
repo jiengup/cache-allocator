f19124a70eb76d8e460c5f30e130c38578929320
root <root@a109-27.local>
2023-07-31 03:02:02 +0000

    modify ignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/cacheAllocation
-# Build directory: /root/cacheAllocation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
